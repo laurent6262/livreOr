95fe3bac9d6e96af7fdacecd49635fd5a45d25f0
laurent febvin <laurentfebvin@gmail.com>
2020-05-22 09:12:44 +0200

    utilisation d'un middleWare: body-parser pour pouvoir traiter le corps de l'objet request

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 let express=require("express")
 let app=express()
+let bodyParser=require("body-parser")
+
+// notre moteur de template
 
 app.set('view engine','ejs')
 
+// paramétrage de l'accès aux fichiers statiques
+
 app.use('/assets',express.static('public'))
+
+// nos middleWares
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// les routes
 
 app.get('/', (request,response)=>{
 
     response.render("pages/index",{test: "salut"})
+
+})
+
+app.post('/', (request,response)=> {
+    
+    console.log(request.body)
 
 })
 
